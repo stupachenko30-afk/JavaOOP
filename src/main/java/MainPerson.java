e638c9e7ab395a5ac9401af6be298095f9d9e25a
Yevhenii <stupachenko30@gmail.com>
2026-05-13 12:40:26 +0200

    add

--- src/main/java/MainPerson.java
+++ src/main/java/MainPerson.java
@@ -1,9 +1,19 @@
 public class MainPerson {
     public static void main(String[] args){
         Person p1 = new Person();//создание объект Person
+        p1.setName("Yevhenii Stupachenko");
+        p1.setId(12345);
+        p1.setAge(46.1);
 
+        Person p2 = new Person();
+        p2.setName("Hanna Cohen");
+        p2.setId(1234567);
+        p2.setAge(34);
     }
 
 
 
 }
+/*
+p1 -> ['Stupachenko .....
+ */
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,7 +1,40 @@
 public class Person {
+    //this ссылка на все что есть в классе
     private String name;
     private int id;
     private double age;
 
+    public void setName(String name) {
+        if (name != null) ;
+        this.name = name;
+    }
+        public void setId(int id) {
+            if (id > 0) ;
+            this.id = id;
+        }
+            public void setAge(double age) {
+                if (age > 0) ;
+                this.age = age;
+            }
 
+
+// getters
+    public String getName(){
+        return  name;
+
+    }
+
+    public int getId() {
+        return id;
+
+
+    }
+    public double getAge(){
+        return age;
+    }
 }
+
+
+
+
+
